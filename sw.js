d69975cb8d7dbe382913d61e8de0bcb0277df231
David Fliesen <dfliesen@gmail.com>
2026-09-25 18:52:28 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,33 +1,3 @@
-// What's 4 Dinner? PWA Cache Service Worker v1.2.0
-const CACHE_NAME = 'w4d-cache-v1.2.0';
-const ASSETS_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  'https://cdn.tailwindcss.com',
-  'https://unpkg.com/@phosphor-icons/web'
-];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => Promise.all(
-      keys.map((k) => { if (k !== CACHE_NAME) return caches.delete(k); })
-    ))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request).catch(() => caches.match('./index.html')))
-  );
-});
+﻿const CACHE_NAME = 'whats-4-dinner-v1.2.2';
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => self.clients.claim());
